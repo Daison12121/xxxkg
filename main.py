e418a55bacf734fe22e04701d21424a34e4851ac
Daison12121 <your-email@example.com>
2025-08-08 21:49:27 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")  # Токен берем из переменных окружения
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Привет")
+
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
