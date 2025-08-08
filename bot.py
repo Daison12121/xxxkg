5103286bd36bc73ea5cd3bac2ff9465feedf831c
Daison12121 <your-email@example.com>
2025-08-09 01:10:58 +0700

    Rename main.py to bot.py

---
+++ bot.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello! I'm alive!")
+
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
